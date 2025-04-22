331b94369e2755a53851a9a11d48a476d497180e
DrunkenPipe <huynhtrongnhanyahoocom@gmail.com>
2025-04-22 14:03:37 +0700

    thêm chức năng check điều kiện nhập

--- BUS/KhachHangBUS.py
+++ BUS/KhachHangBUS.py
@@ -13,19 +13,23 @@ class KhachHangBUS:
         """Tìm kiếm khách hàng"""
         if search_type:
             return self.DAO.search_Customer(keyword, search_type)
-            raise ValueError("Invalid search type")
+        raise ValueError("từ khóa không được bỏ trống!")
     def insert_user(self, name, email, phone, address):
+        if not name or not email or not phone or not address:
+            raise ValueError("Vui lòng điền đầy đủ thông tin khách hàng!")
         # Example validation: check if email is in valid format
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email): #[^@]+ means "one or more characters that are not @(john,nguyen,gmail,com)" && \. means a literal dot. && @ means a literal @
-            raise ValueError("Invalid email format")
+            raise ValueError("email phải theo định dạng ___@__.___")
         # Additional validations can go here:
         # If validation passes, proceed to insert the user
         new_user_id = self.DAO.insert_user(name, email, phone, address)
         return new_user_id
     def update_user(self, id, name, email, phone, address, NgayDangKy):
+        if not name or not email or not phone or not address:
+            raise ValueError("Vui lòng điền đầy đủ thông tin khách hàng!")
         """Updates an existing customer."""
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email): #[^@]+ means "one or more characters that are not @(john,nguyen,gmail,com)" && \. means a literal dot. && @ means a literal @
-            raise ValueError(f"Invalid email format, while trying to input email: {email}")
+            raise ValueError(f"email phải theo định dạng ___@__.___, định dạng được nhập vào: {email}")
         self.DAO.update_user(id, name, email, phone, address, NgayDangKy)
 
     def delete_user(self, id):
